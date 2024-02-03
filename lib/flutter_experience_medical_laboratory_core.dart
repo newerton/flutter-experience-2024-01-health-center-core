7edce7d04e2a7f318450ff823920d34b9537bbf8
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2024-02-03 15:49:02 -0300

    chore: add messages

---
+++ lib/flutter_experience_medical_laboratory_core.dart
@@ -0,0 +1,6 @@
+library flutter_experience_medical_laboratory_core;
+
+export 'src/fp/either.dart';
+export 'src/fp/nil.dart';
+export 'src/fp/unit.dart';
+export 'src/helpers/messages.dart';
---
+++ lib/src/fp/nil.dart
@@ -0,0 +1,5 @@
+class Nil {
+  String toString() => 'Nil{}';
+}
+
+Nil get nil => Nil();
